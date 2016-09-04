6fd6c9ef43f3de5f6903495789fee1c2b46493b0
James Kenny <james.m.kenny@gmail.com>
2016-09-04 09:37:06 +0100

    Azure Service Bus App 1
    App 1 demonstrates writing a message to a service bus.

---
+++ App1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting..");
+
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            
+            QueueClient Client = QueueClient.CreateFromConnectionString(connectionString, "serversncodedemoqueue");
+
+            // Create message, passing a string message for the body.
+            BrokeredMessage message = new BrokeredMessage("");
+
+            // Set some addtional custom app-specific properties.
+            message.Properties["UserCode"] = "dascdcasas";
+            message.Properties["UserId"] = "4550";
+
+            try
+            {
+                // Send message to the queue.
+                Client.Send(message);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+            Console.WriteLine("Complete..");
+            Console.ReadKey();
+        }
+    }
+}
